c851176638e60f065741a231fa210f4e7fe23bff
wnewen <plpl541616@gmail.com>
2023-08-25 23:43:48 +0800

    view rotation in room fix

--- Assets/Scripts/View/ViewRotation.cs
+++ Assets/Scripts/View/ViewRotation.cs
@@ -2,18 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VeiwRotation : MonoBehaviour
+public class ViewRotation : MonoBehaviour
 {
     [SerializeField] private float _rotationSpeed = 5.0f;
     private Camera _camera;
     private Transform _cameraTransform;
     private float _targetRotation = 0.0f;
+    private float _targetAngle = 0.0f;
     private bool _isRotating = false;
+
+    float[] _rotationList = new float[] { 30f, 120f, 210f, 360f };
+    public int _rotationCounter = 0;
+    
+    private GameObject _left;
+    private GameObject _right;
 
     private void Awake() 
     {
         _camera = GameObject.FindObjectOfType<Camera>();
         _cameraTransform = _camera.transform;
+
+        _left = GameObject.Find("Left");
+        _right = GameObject.Find("Right");
     }
 
     void Update()
@@ -21,7 +31,7 @@ public class VeiwRotation : MonoBehaviour
         if (_isRotating)
         {
             /* use Lerp to rotate camera smooth */
-            float step = _rotationSpeed * Time.deltaTime;
+            float step = _rotationSpeed * Time.deltaTime;            
             float currentRotation = _cameraTransform.eulerAngles.y;
             _cameraTransform.eulerAngles = new Vector3(0, Mathf.Lerp(currentRotation, _targetRotation, step), 0);
 
@@ -31,13 +41,29 @@ public class VeiwRotation : MonoBehaviour
                 _isRotating = false;
             }
         }
-    }    
+
+        switch (_rotationCounter)
+        {
+            case 0:
+                _left.SetActive(false);
+                break;
+            case 3:
+                _right.SetActive(false);
+                break;
+            default:
+                _left.SetActive(true);
+                _right.SetActive(true);
+                break;
+        }
+    }   
 
     public void RotateCounterClockwise()
     {
         if (!_isRotating)
         {
-            _targetRotation = _cameraTransform.eulerAngles.y - 90.0f;
+            _rotationCounter = _rotationCounter - 1;
+            _targetAngle = _rotationList[_rotationCounter];
+            _targetRotation = _targetAngle;
             _isRotating = true;
         }
     }
@@ -46,7 +72,9 @@ public class VeiwRotation : MonoBehaviour
     {
         if (!_isRotating)
         {
-            _targetRotation = _cameraTransform.eulerAngles.y + 90.0f;
+            _rotationCounter = _rotationCounter + 1;
+            _targetAngle = _rotationList[_rotationCounter];
+            _targetRotation = _targetAngle;
             _isRotating = true;
         }
     }
